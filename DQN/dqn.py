2d67fd57158ac6ebabc1bf3171701fe04d95bd4e
yuanyang7 <yangyuan95@gmail.com>
2018-03-21 16:36:51 -0700

    dqngpu

--- DQN/dqn.py
+++ DQN/dqn.py
@@ -317,65 +317,65 @@ def deep_q_learning(sess,
         # One step in the environment
         for t in itertools.count():
 
+            with tf.device("/gpu:0"):
+                # Epsilon for this time step
+                epsilon = epsilons[min(total_t, epsilon_decay_steps-1)]
 
-            # Epsilon for this time step
-            epsilon = epsilons[min(total_t, epsilon_decay_steps-1)]
+                # Add epsilon to Tensorboard
+                episode_summary = tf.Summary()
+                episode_summary.value.add(simple_value=epsilon, tag="epsilon")
+                q_estimator.summary_writer.add_summary(episode_summary, total_t)
 
-            # Add epsilon to Tensorboard
-            episode_summary = tf.Summary()
-            episode_summary.value.add(simple_value=epsilon, tag="epsilon")
-            q_estimator.summary_writer.add_summary(episode_summary, total_t)
+                # Maybe update the target estimator
+                if total_t % update_target_estimator_every == 0:
+                    copy_model_parameters(sess, q_estimator, target_estimator)
+                    print("\nCopied model parameters to target network.")
 
-            # Maybe update the target estimator
-            if total_t % update_target_estimator_every == 0:
-                copy_model_parameters(sess, q_estimator, target_estimator)
-                print("\nCopied model parameters to target network.")
+                # Print out which step we're on, useful for debugging.
+                print("\rStep {} ({}) @ Episode {}/{}, loss: {}".format(
+                        t, total_t, i_episode + 1, num_episodes, loss), end="")
+                sys.stdout.flush()
 
-            # Print out which step we're on, useful for debugging.
-            print("\rStep {} ({}) @ Episode {}/{}, loss: {}".format(
-                    t, total_t, i_episode + 1, num_episodes, loss), end="")
-            sys.stdout.flush()
+                # Take a step
+                action_probs = policy(sess, state, epsilon)
+                action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+                next_state, reward, done, _ = env.step(VALID_ACTIONS[action])
+                reward_sum += reward
+                next_state = state_processor.process(sess, next_state)
+                next_state = np.append(state[:,:,1:], np.expand_dims(next_state, 2), axis=2)
 
-            # Take a step
-            action_probs = policy(sess, state, epsilon)
-            action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
-            next_state, reward, done, _ = env.step(VALID_ACTIONS[action])
-            reward_sum += reward
-            next_state = state_processor.process(sess, next_state)
-            next_state = np.append(state[:,:,1:], np.expand_dims(next_state, 2), axis=2)
+                # If our replay memory is full, pop the first element
+                if len(replay_memory) == replay_memory_size:
+                    replay_memory.pop(0)
 
-            # If our replay memory is full, pop the first element
-            if len(replay_memory) == replay_memory_size:
-                replay_memory.pop(0)
+                # Save transition to replay memory
+                replay_memory.append(Transition(state, action, reward, next_state, done))   
 
-            # Save transition to replay memory
-            replay_memory.append(Transition(state, action, reward, next_state, done))   
+                # Update statistics
+                stats.episode_rewards[i_episode] += reward
+                stats.episode_lengths[i_episode] = t
 
-            # Update statistics
-            stats.episode_rewards[i_episode] += reward
-            stats.episode_lengths[i_episode] = t
+                # Sample a minibatch from the replay memory
+                samples = random.sample(replay_memory, batch_size)
+                states_batch, action_batch, reward_batch, next_states_batch, done_batch = map(np.array, zip(*samples))
 
-            # Sample a minibatch from the replay memory
-            samples = random.sample(replay_memory, batch_size)
-            states_batch, action_batch, reward_batch, next_states_batch, done_batch = map(np.array, zip(*samples))
+                # Calculate q values and targets (Double DQN)
+                q_values_next = q_estimator.predict(sess, next_states_batch)
+                best_actions = np.argmax(q_values_next, axis=1)
+                q_values_next_target = target_estimator.predict(sess, next_states_batch)
+                targets_batch = reward_batch + np.invert(done_batch).astype(np.float32) * \
+                    discount_factor * q_values_next_target[np.arange(batch_size), best_actions]
 
-            # Calculate q values and targets (Double DQN)
-            q_values_next = q_estimator.predict(sess, next_states_batch)
-            best_actions = np.argmax(q_values_next, axis=1)
-            q_values_next_target = target_estimator.predict(sess, next_states_batch)
-            targets_batch = reward_batch + np.invert(done_batch).astype(np.float32) * \
-                discount_factor * q_values_next_target[np.arange(batch_size), best_actions]
+                # Perform gradient descent update
+                states_batch = np.array(states_batch)
+                loss = q_estimator.update(sess, states_batch, action_batch, targets_batch)
 
-            # Perform gradient descent update
-            states_batch = np.array(states_batch)
-            loss = q_estimator.update(sess, states_batch, action_batch, targets_batch)
+                if done:
+                    break
 
-            if done:
-                break
-
-            state = next_state
-            total_t += 1
-           # env.render()
+                state = next_state
+                total_t += 1
+               # env.render()
 
         # Add summaries to tensorboard
         episode_summary = tf.Summary()
